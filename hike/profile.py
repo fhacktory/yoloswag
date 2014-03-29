121c0fe81c266ac67a22eccf678033122d11e8a9
Luis <jean-louis.darmon@epitech.eu>
2014-03-29 20:32:04 +0100

    QF

--- hike/profile.py
+++ hike/profile.py
@@ -1,8 +1,10 @@
 from hike import app
+from flask import abort, render_template
+
 
 @app.route('/user/<idUser:int>')
 def user_profile(idUser = 0):
-    if idUser == 0
+    if idUser == 0:
         abort(404)
     return render_template("index.html", view="profile.html")
 
