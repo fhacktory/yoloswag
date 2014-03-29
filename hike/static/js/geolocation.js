bf079119ba0e386087bc0937e2b7ad59aace2bfa
Wilfried <wilfried@tacquard.eu>
2014-03-29 21:20:46 +0100

    Add cursor on current location

--- hike/static/js/geolocation.js
+++ hike/static/js/geolocation.js
@@ -5,6 +5,13 @@ function geolocation_onError(error){
 function geolocation_onSuccess(position){
     map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
     map.setZoom(15);
+    var marker = new google.maps.Marker({
+	draggable:false,
+	animation: google.maps.Animation.DROP,
+	position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        title: "You"
+    });
+    marker.setMap(map);
 }
 
 function geolocation_setCurrentPosition()
