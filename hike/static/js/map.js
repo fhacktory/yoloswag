060fa0a4f9e2f03ccbaac1d6f6d86b033b23cb08
Wilfried <wilfried@tacquard.eu>
2014-03-30 09:48:28 +0200

    Refresh on radius change (aith a fucking slider and leaks !

--- hike/static/js/map.js
+++ hike/static/js/map.js
@@ -1,6 +1,9 @@
 var map;
 var elevator;
-var currentPosition
+var currentPosition;
+
+var gpaths = new Array();
+var gmarkers = new Array();
 
 function initialize() {
     var mapOptions = {
@@ -11,6 +14,23 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     elevator = new google.maps.ElevationService();
     currentPosition = new google.maps.LatLng(45.750000, 4.850000);
+    geolocation_setCurrentPosition();
     onLoad();
 }
+
+function refresh()
+{
+    console.log("REFRESSH");
+    $.each(gpaths, function(i, path) {
+	path.setMap(null);
+//	delete path;
+    });
+    $.each(gmarkers, function(i, mark) {
+	mark.setMap(null);
+//	delete mark;
+    });
+    console.log("RELOAD");
+    onLoad();
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
